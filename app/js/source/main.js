07769040600472698d12b05685235650b000cb1c
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-04-14 12:02:39 -0500

    lecture part 2

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -9,6 +9,8 @@
     $('.clear').click(clear);
     $('.decimal').click(decimal);
     $('#convert').click(convert);
+    $('.push').click(push);
+    $('.operator').click(operator);
   }
 
   function number(){
@@ -29,6 +31,8 @@
     var cleartype = this.textContent;
     if (cleartype === 'C') {
       $('#display').text(0);
+    } else if (cleartype === 'X') {
+      $('#queue').empty();
     }
   }
 
@@ -56,6 +60,73 @@
     } else {
       $('#container').fadeOut();
     }
+  }
+
+  function push(){
+    var display = $('#display').text();
+    $('#display').text(0);
+    var $div = $('<div>');
+    $div.text(display);
+    $('#queue').prepend($div);
+  }
+
+  function operator(){
+    var op = $(this).data('op');
+    var x = $('#queue > div:nth-child(1)').text() * 1;
+    var y = $('#queue > div:nth-child(2)').text() * 1;
+
+    switch(op) {
+      case 'add':
+        var result = x + y;
+      break;
+      case 'sub':
+        result = x - y;
+      break;
+      case 'mult':
+        result = x * y;
+      break;
+      case 'div':
+        result = x / y;
+      break;
+      case 'exp':
+        result = Math.pow(y, x);
+      break;
+      case 'root':
+        result = Math.sqrt(x);
+      break;
+      case 'fact':
+        result = factorial(x);
+      break;
+      case 'sum':
+        result = sum();
+
+    }
+
+    $('#display').text(result);
+    $('#queue').empty();
+
+  }
+
+  function sum(){
+    var total = 0;
+
+    $('#queue > div').each(function(index, div){
+      var x = div.textContent * 1;
+      total = total + x;
+    });
+
+    return total;
+  }
+
+  function factorial(x){
+    var product = 1;
+
+    for(var i = 2; i <= x; i++){
+      product = product * i;
+    }
+    console.log(product);
+
+    return product;
   }
 
 })();
